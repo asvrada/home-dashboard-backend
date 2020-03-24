f5fdad6085ca95b5b434fa426740a192781b2cf5
Zijie Wu <noojeff@gmail.com>
2020-03-23 22:49:59 -0700

    reformat

--- restful/tst/setup.py
+++ restful/tst/setup.py
@@ -5,38 +5,36 @@ from rest_framework.test import APIClient, APITestCase
 
 def create_dummy_transactions():
     client = APIClient()
-    response = client.post(reverse("bill-list"), data={
-        "icon": "icon 1",
-        "amount": -12.00,
-        "category": "食物",
-        "company": "DoorDash",
-        "card": "BOA",
-        "note": "中饭",
-    }, format="json")
+    transactions = [
+        {
+            "icon": "icon 1",
+            "amount": -12.00,
+            "category": "食物",
+            "company": "DoorDash",
+            "card": "BOA",
+            "note": "中饭",
+        },
+        {
+            "icon": "icon 2",
+            "amount": -1099,
+            "category": "电子",
+            "company": "Valve",
+            "card": "discover",
+            "note": "Index VR",
+        },
+        {
+            "icon": "icon 3",
+            "amount": 12.00,
+            "category": "stock",
+            "company": "apple",
+            "card": "BOA",
+            "note": "stock income",
+        }
+    ]
 
-    assert response.status_code == status.HTTP_201_CREATED, response.content
-
-    response = client.post(reverse("bill-list"), data={
-        "icon": "icon 2",
-        "amount": -1099,
-        "category": "电子",
-        "company": "Valve",
-        "card": "discover",
-        "note": "Index VR",
-    }, format="json")
-
-    assert response.status_code == status.HTTP_201_CREATED, response.content
-
-    response = client.post(reverse("bill-list"), data={
-        "icon": "icon 3",
-        "amount": 12.00,
-        "category": "stock",
-        "company": "apple",
-        "card": "BOA",
-        "note": "stock income",
-    }, format="json")
-
-    assert response.status_code == status.HTTP_201_CREATED, response.content
+    for payload in transactions:
+        response = client.post(reverse("bill-list"), data=payload, format="json")
+        assert response.status_code == status.HTTP_201_CREATED, response.content
 
 
 class BasicAPITestCase(APITestCase):
