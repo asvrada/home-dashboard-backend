2dccc278938c817d7c16d0efbe4e024b15117df5
Zijie Wu <noojeff@gmail.com>
2020-03-26 22:01:57 -0700

    Finished summary API

--- restful/tst/test_summary.py
+++ restful/tst/test_summary.py
@@ -3,13 +3,20 @@ from rest_framework.test import APIClient, APITestCase
 from django.urls import reverse
 from rest_framework import status
 
+import pytz
+from unittest import mock
+from datetime import datetime
+
 from .setup import BasicAPITestCase
 
 
 class SummaryTest(BasicAPITestCase):
     def test_get_summary(self):
-        response = self.client.get(reverse('summary-get'))
+        # Mock today to be 2020/3/20, 12 days till next month
+        mocked = datetime(2020, 3, 20, 12, 0, 0, tzinfo=pytz.utc)
+        with mock.patch('django.utils.timezone.now', mock.Mock(return_value=mocked)):
+            response = self.client.get(reverse('summary-get'))
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(type(response.data), dict)
-        self.assertEqual(response.data, {'total': -1099, 'remainingTotal': 1200, 'remainingToday': 200})
+        self.assertEqual(response.data, {'total': 7012, 'budgetTotal': 6969, 'budgetToday': 581})
--- restful/views.py
+++ restful/views.py
@@ -3,6 +3,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from django.db.models import Sum
+from django.utils import timezone
+
+from calendar import monthrange
 
 from . import models
 from . import serializers
@@ -21,16 +24,46 @@ class SummaryView(APIView):
         """
         Calculate the sum of income/spend this month
         total: sum of income this month
-        remainingTotal: {total} - sum of spend this month
-        remainingToday: ({total} - sum of spend (except today)) / days till end of month
+        budgetTotal: budget left for this month
+        budgetToday: budget left for today
         """
-        total_amount = models.Transaction.objects.all().aggregate(tmp_total=Sum('amount'))["tmp_total"]
+        # Get current date
+        today = timezone.now()
+        year, month, day = today.year, today.month, today.day
+
+        bill_month = models.Transaction.objects.filter(time_created__year=year, time_created__month=month)
+        bill_income_month = bill_month.filter(amount__gte=0)
+        bill_today = bill_month.filter(time_created__day=day)
+
+        # Sum of all income this month
+        sum_income_month = self.aggregate_amount(bill_income_month)
+        # Sum of all spending this month
+        # A negative number
+        sum_this_month = self.aggregate_amount(bill_month.filter(amount__lte=0))
+        # Sum of all spending today
+        # A negative number
+        sum_today = self.aggregate_amount(bill_today.filter(amount__lte=0))
+
+        # Days left for this month
+        _, days_month = monthrange(year, month)
+        # Days left, exclude today
+        # Image its Jan, For 1st, there will 31days, for 31st, there will be 1 day
+        # Also make sure its >= 1
+        days_left = max(1, days_month - day + 1)
 
         return Response(data={
-            "total": total_amount,
-            "remainingTotal": 1200,
-            "remainingToday": 200
+            "total": self.convert_float(sum_income_month),
+            "budgetTotal": self.convert_float(sum_income_month + sum_this_month),
+            "budgetToday": self.convert_float((sum_income_month + sum_this_month - sum_today) / days_left)
         })
+
+    @staticmethod
+    def aggregate_amount(queryset):
+        return queryset.aggregate(tmp_total=Sum('amount'))["tmp_total"] or 0
+
+    @staticmethod
+    def convert_float(number):
+        return round(number)
 
 
 class TransactionViewSet(viewsets.ModelViewSet):
